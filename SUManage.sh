d50adebdd34f2897e751d47184789a27ab119eb1
Simon <SCTCoding@users.noreply.github.com>
2022-05-07 00:05:20 -0400

    Added license
    Added license header.

--- SUManage.sh
+++ SUManage.sh
@@ -9,7 +9,23 @@
 #                                             /____/        #  
 #############################################################
 
-## Copyright April 26, 2022 Simon
+#############################################################
+## Create by Simon Carlson-Thies on 4/21/22
+## Copyright © 2022 Simon Carlson-Thies All rights reserved.
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#############################################################
 
 loggedInUser=$(/usr/bin/stat -f%Su /dev/console)
 loggedInUID=$(/usr/bin/id -u "$loggedInUser")
